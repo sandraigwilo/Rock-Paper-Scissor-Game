44a7152de9f2202ba1651c675b0ab5f8784d3943
Sandra Ngozi Igwilo <105286471+sandraigwilo@users.noreply.github.com>
2022-06-11 13:11:54 +0100

    Delete Adding_integers.py

--- Adding_integers.py
+++
@@ -1,7 +0,0 @@
-# This program adds two integers
-integer1 = 56
-integer2 = 44
-# Add two integers
-sum = integer1 + integer2
-# Displays the sum
-print("The sum of {0} and {1} is {2}".format (integer1, integer2, sum))
